961cffd36d4ba86b409ee581e0a13440e09126be
Scott Jones <scott@stackless.dev>
2023-03-17 07:31:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ChatExample
+Example real-time CLI-based chat server and client application using Estate.
+
+# Setup
+1. cd service/
+2. service init
+3. service boot
+4. cd ../client
+5. service connect <the service name you chose at step 3>
+
+# Run
+6. (in one terminal) npm start --console
+7. (in second terminal) npm start
---
+++ index.mjs
@@ -0,0 +1,54 @@
+import inquirer from 'inquirer';
+import chalk from "chalk";
+
+import { setOptions, getWorker, subscribeMessageAsync, ServiceOptions } from "estate-js";
+import { ChatServer, ChatTextSaid } from "chat-service";
+
+//hide the normally verbose logging
+setOptions(new ServiceOptions(false, false));
+
+const chatServer = getWorker(ChatServer, "Scott's No-kill Animal Shelter");
+
+console.log(`Welcome to ${chalk.blueBright(chatServer.primaryKey)}!`)
+console.log(chalk.grey(`The current server time is ${await chatServer.getTimeAsync()}.`));
+
+const args = process.argv.slice(2);
+if (args[0] === "--console") {
+    console.log("Press Ctrl+C to exit.")
+    const history = await chatServer.getHistoryAsync();
+    if (history && history.length) {
+        for (const chatText of history)
+            outputChatText(chatText);
+    }
+    await subscribeMessageAsync(chatServer, ChatTextSaid, (chatTextSaid) => {
+        outputChatText(chatTextSaid.chatText);
+    });
+} else {
+    await inquirer.prompt([{
+        type: 'input',
+        name: 'userName',
+        message: "Login: "
+    }]).then(async ({userName}) => {
+        console.log("Use /quit to exit chat.");
+        while (true) {
+            await inquirer.prompt([{
+                type: 'input',
+                name: 'text',
+                message: `${userName}: `
+            }]).then(async ({text}) => {
+                if (text === "/quit") {
+                    process.exit(0);
+                    return;
+                }
+                await chatServer.sayAsync(userName, text);
+            })
+        }
+    })
+}
+
+function outputChatText(chatText) {
+    const timestamp = chalk.grey(`[${chatText.timestamp.toLocaleTimeString()}]`);
+    const userName = chalk.whiteBright(`[${chatText.userName}]:`);
+    const text = chalk.greenBright(chatText.text);
+    console.log(`${timestamp} ${userName} ${text}`);
+}
