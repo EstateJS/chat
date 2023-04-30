58a8e457ea07e8edf51158856c7d8d676fd98962
Scott Jones <scott@stackless.dev>
2023-04-30 14:56:23 -0400

    upgraded chat example to typescript

--- service/index.ts
+++ service/index.ts
@@ -1,73 +1,38 @@
 import {createUuid, saveData, sendMessage, Message, Data, Worker} from "./estate-runtime";
 
-class User extends Data {
-    constructor(name) {
-        super(name);
-        this._creationDate = new Date();
-        this._history = [];
-        this._activeChannel = null;
-        this._chatServer = null;
-    }
-    get creationDate() { return this._creationDate; }
-    get history() { return this._history; }
-    get chatServer() {
-        return this._chatServer;
-    }
-    set chatServer(value) {
-        this._chatServer = value;
-    }
-    setServer(chatServer) {
-        this._chatServer = chatServer;
-    }
-
-}
-
-class ChatServer extends Worker {
-    constructor(name) {
+export class ChatServer extends Worker {
+    private _history: ChatEntry[];
+    constructor(name: string) {
         super(name);
         this._history = [];
     }
 
-    say(userName, text) {
-        const chatText = new ChatEntry(userName, text);
-        this._history.push(chatText);
-        saveData(chatText); // Save data object to datastore
-        sendMessage(this, new ChatTextSaid(chatText)); // Send SSE to listening clients
+    say(userName: string, text: string) {
+        const entry = new ChatEntry(userName, text);
+        this._history.push(entry);
+        saveData(entry); // Save data object to datastore
+        sendMessage(this, new ChatMessage(entry)); // Send SSE to listening clients
     }
 
-    getHistory() {
+    getHistory() : ChatEntry[] {
         return this._history;
     }
 
-    getTime() {
+    getTime() : Date {
         return new Date();
     }
 }
 
-class ChatEntry extends Data {
-    constructor(userName, text) {
-        super(createUuid());
-        this._userName = userName;
-        this._text = text;
-        this._timestamp = new Date();
-    }
-    get userName() {
-        return this._userName;
-    }
-    get timestamp() {
-        return this._timestamp;
-    }
-    get text() {
-        return this._text;
+export class ChatEntry extends Data {
+    public readonly timestamp: Date;
+    constructor(public readonly userName: string, public readonly text: string) {
+        super(createUuid(false));
+        this.timestamp = new Date();
     }
 }
 
-class ChatMessage extends Message {
-    constructor(chatText) {
+export class ChatMessage extends Message {
+    constructor(public entry: ChatEntry) {
         super();
-        this._chatText = chatText;
-    }
-    get chatText() {
-        return this._chatText;
     }
 }
